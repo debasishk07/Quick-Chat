f341b434878bb1f4d54c043f5b94c371b8fc675e
Debasish <debasishk7809@gmail.com>
2026-07-09 10:31:45 +0530

    added .gitignore

--- android/app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_quickchat_app_QuickChatApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.quickchat.app.QuickChatApplication"
-)
-public final class _com_quickchat_app_QuickChatApplication {
-}
